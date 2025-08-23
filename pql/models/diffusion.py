5bdc3314ad18198fcd33c9883174dd8ba7a14db6
Steven Li <zl2993@columbia.edu>
2025-08-23 13:09:25 +0200

    newest diffusion

--- pql/models/diffusion.py
+++ pql/models/diffusion.py
@@ -4,6 +4,7 @@ import math
 import torch
 import torch.nn as nn
 from diffusers.schedulers.scheduling_ddpm import DDPMScheduler
+from diffusers.schedulers.scheduling_ddim import DDIMScheduler
 
 
 class SinusoidalPosEmb(nn.Module):
@@ -20,6 +21,68 @@ class SinusoidalPosEmb(nn.Module):
         emb = torch.cat((emb.sin(), emb.cos()), dim=-1)
         return emb
     
+def zeromodule(module):
+    """
+    Zero out the parameters of a module and return it.
+    """
+    for p in module.parameters():
+        p.detach().zero_()
+    return module
+
+def fourier_encode(x: torch.Tensor, num_freqs: int, max_freq: float, include_input: bool=False) -> torch.Tensor:
+    """x: [B, C] -> [B, C*(2F) (+C if include_input)]，对数间隔频率 + 幅度归一"""
+    B, C = x.shape
+    freqs = torch.exp(torch.linspace(0.0, math.log(max_freq + 1e-6), num_freqs, device=x.device, dtype=x.dtype))
+    xb = x.unsqueeze(-1) * (2.0 * math.pi) * freqs          # [B,C,F]
+    enc = torch.cat([torch.sin(xb), torch.cos(xb)], dim=-1) # [B,C,2F]
+    enc = enc.reshape(B, C * (2 * num_freqs)) / math.sqrt(num_freqs)
+    return torch.cat([x, enc], dim=-1) if include_input else enc
+
+class FourierSE3Embedder(nn.Module):
+    """
+    输入:  T:[B,4,4]
+    流程:  R->6D，与 t(平移) 拼接 -> (可选 Fourier) -> MLP -> [B,d_model]
+    """
+    def __init__(
+        self,
+        state_dim: int,
+        d_model: int,
+        hidden: int = 256,
+        num_freqs: int = 6,
+        max_freq: float = 12.0,
+        dropout: float = 0.0,
+        ln_before: bool = True,       # 在MLP前做LayerNorm
+    ):
+        super().__init__()
+        self.num_freqs = num_freqs
+        self.max_freq = max_freq
+
+        in_dim_raw = state_dim
+        in_dim = (in_dim_raw * (2*num_freqs) + in_dim_raw)
+
+        self.norm_in = nn.LayerNorm(in_dim) if ln_before else nn.Identity()
+        self.mlp = nn.Sequential(
+            nn.Linear(in_dim, hidden),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden, d_model),
+        )
+
+        # 小初始化，避免一开始过强
+        for m in self.mlp:
+            if isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, mean=0.0, std=0.05)
+                nn.init.zeros_(m.bias)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        T: [B,44] -> [B,d_model]
+        """
+        x = fourier_encode(x, self.num_freqs, self.max_freq, include_input=True)  # [B, 9+18*num_freqs]
+
+        x = self.norm_in(x)
+        out = self.mlp(x)  # [B,d_model]
+        return out
 
 class DiffusionNet(nn.Module):
     def __init__(
@@ -28,7 +91,9 @@ class DiffusionNet(nn.Module):
         cond_dim,
         dim=256,
         num_blocks=3,
-        act_fn=nn.Mish()
+        act_fn=nn.Mish(),
+        multi_head=False,
+        split_obs=False
     ):
         super().__init__()
 
@@ -46,9 +111,32 @@ class DiffusionNet(nn.Module):
         self.action_dim = transition_dim - cond_dim
 
         embed_dim = dim
-
-        self.mlp = nn.Sequential(
-                        nn.Linear(embed_dim + transition_dim, 1024),
+        self.multi_head = multi_head
+        self.split_obs = split_obs
+        if self.multi_head:
+            self.action_dim = self.action_dim // 2
+            self.transition_dim = self.transition_dim - self.action_dim
+            self.mlp1 = nn.Sequential(
+                            nn.Linear(embed_dim + self.transition_dim, 1024),
+                            act_fn,
+                            nn.Linear(1024, 512),
+                            act_fn,
+                            nn.Linear(512, 256),
+                            act_fn,
+                            zeromodule(nn.Linear(256, self.action_dim)),
+                        )
+            self.mlp2 = nn.Sequential(
+                        nn.Linear(embed_dim + self.transition_dim, 1024),
+                        act_fn,
+                        nn.Linear(1024, 512),
+                        act_fn,
+                        nn.Linear(512, 256),
+                        act_fn,
+                        zeromodule(nn.Linear(256, self.action_dim)),
+            )
+        else:
+            self.mlp = nn.Sequential(
+                        nn.Linear(embed_dim + self.transition_dim, 1024),
                         act_fn,
                         nn.Linear(1024, 512),
                         act_fn,
@@ -56,20 +144,29 @@ class DiffusionNet(nn.Module):
                         act_fn,
                         nn.Linear(256, self.action_dim),
                     )
-        
         # self.mlp = MLPResNet(in_dim=embed_dim + transition_dim, out_dim=self.action_dim, num_blocks=num_blocks, act=nn.Mish(), hidden_dim=dim)
 
-    def forward(self, x, time, cond):
+    def forward(self, x, time, cond, cond2=None):
         '''
             x : [ batch x action ]
             cond: [batch x state]
+            cond2: [batch x state]
             returns : [batch x 1]
         '''
         t = self.time_mlp(time)
 
-        inp = torch.cat([t, cond, x], dim=-1)
-        out = self.mlp(inp)
-
+        if self.multi_head:
+            inp1 = torch.cat([t, cond, x[:, :22]], dim=-1)
+            if self.split_obs:
+                inp2 = torch.cat([t, cond2, x[:, 22:]], dim=-1)
+            else:
+                inp2 = torch.cat([t, cond, x[:, 22:]], dim=-1)
+            out1 = self.mlp1(inp1)
+            out2 = self.mlp2(inp2)
+            return out1, out2
+        else:
+            inp = torch.cat([t, cond, x], dim=-1)
+            out = self.mlp(inp)
         return out
 
 
@@ -131,7 +228,15 @@ class MLPResNet(nn.Module):
 from pql.models.pointnet import PointNetEncoderXYZ, MultiStagePointNetEncoder
 from pql.models.visual import weight_init
 class DiffusionPolicy(nn.Module):
-    def __init__(self, state_dim, action_dim, diffusion_iter, device="cuda"):
+    def __init__(self, state_dim, action_dim, diffusion_iter, 
+                 point_encoder_type="xyz", 
+                 point_encoder_out_channels=256,
+                 state_encoder_out_channels=256,
+                 multi_head=False,
+                 split_obs=False,
+                 normalizer=None,
+                 use_fourier=False,
+                 device="cuda"):
         super().__init__()
         if isinstance(state_dim, Sequence):
             state_dim = state_dim[0]
@@ -139,28 +244,81 @@ class DiffusionPolicy(nn.Module):
         self.action_dim = action_dim
         self.diffusion_iter = diffusion_iter
         self.device = device
-        self.point_encoder = PointNetEncoderXYZ(in_channels=3,
-                                                out_channels=256,
-                                                use_layernorm=True,
-                                                final_norm='layernorm',
-                                                use_projection=True)
-        # self.point_encoder = MultiStagePointNetEncoder(out_channels=128)
-        self.point_encoder.apply(weight_init)
+        self.multi_head = multi_head
+        self.split_obs = split_obs
+        self.normalizer = normalizer
+        if point_encoder_type == "xyz":
+            self.point_encoder_func = PointNetEncoderXYZ
+        elif point_encoder_type == "multistage":
+            self.point_encoder_func = MultiStagePointNetEncoder
+        else:
+            raise ValueError(f"Invalid point encoder type: {point_encoder_type}")
         # self.obs_encoder = nn.Identity()
-        self.obs_encoder = nn.Sequential(nn.Linear(state_dim, 256), nn.ReLU(inplace=True), nn.Linear(256, 256), nn.LayerNorm(256))
-        self.obs_encoder.apply(weight_init)
+        self.point_encoder = self.point_encoder_func(out_channels=point_encoder_out_channels)
+        self.point_encoder.apply(weight_init)
+        if self.split_obs:
+            state_dim = state_dim // 2
+            # self.point_encoder1 = self.point_encoder_func(out_channels=point_encoder_out_channels)
+            # self.point_encoder1.apply(weight_init)
+            # self.point_encoder2 = self.point_encoder_func(out_channels=point_encoder_out_channels)
+            # self.point_encoder2.apply(weight_init)
+            if use_fourier:
+                self.obs_encoder1 = FourierSE3Embedder(state_dim=state_dim, d_model=state_encoder_out_channels, num_freqs=6, max_freq=12.0)
+                self.obs_encoder1.apply(weight_init)
+                self.obs_encoder2 = FourierSE3Embedder(state_dim=state_dim, d_model=state_encoder_out_channels, num_freqs=6, max_freq=12.0)
+                self.obs_encoder2.apply(weight_init)
+            else:
+                self.obs_encoder1 = nn.Sequential(nn.Linear(state_dim, state_encoder_out_channels), 
+                                                nn.ReLU(inplace=True), 
+                                                nn.Linear(state_encoder_out_channels, state_encoder_out_channels), 
+                                                nn.LayerNorm(state_encoder_out_channels))
+                self.obs_encoder1.apply(weight_init)
+                self.obs_encoder2 = nn.Sequential(nn.Linear(state_dim, state_encoder_out_channels), 
+                                            nn.ReLU(inplace=True), 
+                                            nn.Linear(state_encoder_out_channels, state_encoder_out_channels), 
+                                            nn.LayerNorm(state_encoder_out_channels))
+                self.obs_encoder2.apply(weight_init)
+        else:
+            # self.point_encoder = self.point_encoder_func(out_channels=point_encoder_out_channels)
+            # self.point_encoder.apply(weight_init)
+            if use_fourier:
+                self.obs_encoder = FourierSE3Embedder(state_dim=state_dim, d_model=state_encoder_out_channels, num_freqs=6, max_freq=12.0)
+                self.obs_encoder.apply(weight_init)
+            else:
+                self.obs_encoder = nn.Sequential(nn.Linear(state_dim, state_encoder_out_channels), 
+                                                nn.ReLU(inplace=True), 
+                                                nn.Linear(state_encoder_out_channels, state_encoder_out_channels), 
+                                                nn.LayerNorm(state_encoder_out_channels))
+                self.obs_encoder.apply(weight_init)
 
         # init network
         self.net = DiffusionNet(
-            transition_dim=self.point_encoder.out_channels + action_dim + 256,
-            cond_dim=self.point_encoder.out_channels + 256)
+            transition_dim=point_encoder_out_channels + action_dim + state_encoder_out_channels,
+            cond_dim=point_encoder_out_channels + state_encoder_out_channels,
+            multi_head=multi_head
+        )
+        # self.net = DiffusionNet(
+        #     transition_dim=action_dim + state_encoder_out_channels,
+        #     cond_dim=state_encoder_out_channels,
+        #     multi_head=multi_head
+        # )
 
         # init noise scheduler
-        self.noise_scheduler = DDPMScheduler(
-            num_train_timesteps=self.diffusion_iter,
+        # self.noise_scheduler = DDPMScheduler(
+        #     num_train_timesteps=self.diffusion_iter,
+        #     beta_schedule='squaredcos_cap_v2',
+        #     # clip output to [-1,1] to improve stability
+        #     clip_sample=True,
+        #     prediction_type='epsilon'
+        # )
+        self.noise_scheduler = DDIMScheduler(
+            num_train_timesteps=15,
+            beta_start=0.0001,
+            beta_end=0.02,
             beta_schedule='squaredcos_cap_v2',
-            # clip output to [-1,1] to improve stability
             clip_sample=True,
+            set_alpha_to_one=True,
+            steps_offset=0,
             prediction_type='epsilon'
         )
 
@@ -170,8 +328,17 @@ class DiffusionPolicy(nn.Module):
     def get_actions(self, img, state, pc, sample=True):
         B = state.shape[0]
         point_feat = self.point_encoder(pc)
-        obs_feat = self.obs_encoder(state)
-        point_state_feat = torch.cat([point_feat, obs_feat], dim=-1)
+        if self.split_obs:
+            # point_feat1 = self.point_encoder1(pc[:, :, :3])
+            # point_feat2 = self.point_encoder2(pc[:, :, 3:])
+            obs_feat1 = self.obs_encoder1(state[:, :22])
+            obs_feat2 = self.obs_encoder2(state[:, 22:])
+            point_state_feat = torch.cat([point_feat, obs_feat1], dim=-1)
+            point_state_feat2 = torch.cat([point_feat, obs_feat2], dim=-1)
+        else:
+            obs_feat = self.obs_encoder(state)
+            point_state_feat = torch.cat([point_feat, obs_feat], dim=-1)
+        # point_state_feat = self.obs_encoder(state)
         # init action from Guassian noise
         noisy_action = torch.randn(
             (B, self.action_dim), device=self.device)
@@ -185,8 +352,13 @@ class DiffusionPolicy(nn.Module):
             noise_pred = self.net(
                 noisy_action,
                 timesteps,
-                point_state_feat
+                point_state_feat,
+                point_state_feat2 if self.split_obs else None
             )
+            if self.multi_head:
+                noise_pred1, noise_pred2 = noise_pred
+                noise_pred = torch.cat([noise_pred1, noise_pred2], dim=-1)
+
             if sample:
                 noise_pred = noise_pred.detach()
 
@@ -199,10 +371,16 @@ class DiffusionPolicy(nn.Module):
             if sample:
                 noisy_action = noisy_action.detach()
 
+        if self.normalizer is not None:
+            noisy_action = self.normalizer['action'].unnormalize(noisy_action)
+
         return noisy_action
         
     def get_loss(self, img, state, pc, action, noise=None, timesteps=None):
         B = action.shape[0]
+        if self.normalizer is not None:
+            action = self.normalizer['action'].normalize(action)
+
         # sample noise to add to actions
         if noise is None:
             noise = torch.randn_like(action, device=self.device)
@@ -219,16 +397,34 @@ class DiffusionPolicy(nn.Module):
         noisy_action = self.noise_scheduler.add_noise(
             action, noise, timesteps)
         point_feat = self.point_encoder(pc)
-        obs_feat = self.obs_encoder(state)
-        point_state_feat = torch.cat([point_feat, obs_feat], dim=-1)
-
+        if self.split_obs:
+            obs_feat1 = self.obs_encoder1(state[:, :22])
+            obs_feat2 = self.obs_encoder2(state[:, 22:])
+            point_state_feat = torch.cat([point_feat, obs_feat1], dim=-1)
+            point_state_feat2 = torch.cat([point_feat, obs_feat2], dim=-1)
+        else:
+            obs_feat = self.obs_encoder(state)
+            point_state_feat = torch.cat([point_feat, obs_feat], dim=-1)
+        # point_state_feat = self.obs_encoder(state)
         # predict the noise residual
         noise_pred = self.net(
                 noisy_action,
                 timesteps,
-                point_state_feat
+                point_state_feat,
+                point_state_feat2 if self.split_obs else None
             )
-            
-        # L2 loss
-        loss = nn.functional.mse_loss(noise_pred, noise)
-        return loss
+        pred_type = self.noise_scheduler.config.prediction_type 
+        if pred_type == 'epsilon':
+            target = noise
+        elif pred_type == 'sample':
+            target = action
+
+        if self.multi_head:
+            noise_pred1, noise_pred2 = noise_pred
+            loss1 = nn.functional.mse_loss(noise_pred1, target[:, :22], reduction='none').mean(dim=1)  # [B]
+            loss2 = nn.functional.mse_loss(noise_pred2, target[:, 22:], reduction='none').mean(dim=1)  # [B]
+            loss = ((loss1 + loss2) / 2).mean()  # scalar 
+            return loss, loss1.mean(), loss2.mean()
+        else:
+            loss = nn.functional.mse_loss(noise_pred, target)
+            return loss
